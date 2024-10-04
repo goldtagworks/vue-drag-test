5a809e2fb4278e3f067bcab2fc40ff11aa921bd5
ian.lab(jaewon.lee)/melon <ian.lab@kakaoent.com>
2024-10-04 20:03:53 +0900

    테스트

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,35 @@
 <template>
-    <div class="flex">
-        <VueDraggable class="q-pa-md q-ma-md bg-brown-2" v-model="list1" :animation="animation" ghostClass="ghost" group="people" @update="onUpdate" @add="onAdd" @remove="remove">
-            <div v-for="item in list1" :key="item.id" class="q-pa-md q-ma-md bg-brown-8 text-grey-1">
-                {{ item.name }}
-            </div>
-        </VueDraggable>
-        <VueDraggable class="q-pa-md q-ma-md bg-brown-2" v-model="list2" :animation="animation" group="people" ghostClass="ghost" @update="onUpdate" @add="onAdd" @remove="remove">
-            <div v-for="item in list2" :key="item.id" class="q-pa-md q-ma-md bg-brown-8 text-grey-1">
-                {{ item.name }}
-            </div>
-        </VueDraggable>
+    <div class="q-pa-md row items-start q-gutter-md">
+        <q-card>
+            <q-card-section>
+                <q-list bordered separator>
+                    <VueDraggable v-model="list1" :animation="150" group="people" @update="onUpdate" @add="onAdd" @remove="remove">
+                        <q-item clickable v-ripple v-for="item in list1" :key="item.id" @click="onClicked()">
+                            <q-item-section>
+                                <q-item-label>{{ item.name }}</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                    </VueDraggable>
+                </q-list>
+            </q-card-section>
+        </q-card>
+
+        <q-card>
+            <q-card-section>
+                <q-list bordered separator>
+                    <VueDraggable v-model="list2" :animation="150" handle=".handle" group="people" @update="onUpdate" @add="onAdd" @remove="remove">
+                        <q-item clickable v-ripple v-for="item in list2" :key="item.id">
+                            <q-item-section side>
+                                <q-icon name="drag_handle" color="green" class="handle" />
+                            </q-item-section>
+                            <q-item-section>
+                                <q-item-label>{{ item.name }}</q-item-label>
+                            </q-item-section>
+                        </q-item>
+                    </VueDraggable>
+                </q-list>
+            </q-card-section>
+        </q-card>
     </div>
     <div class="flex justify-between">
         <preview-list :list="list1" />
@@ -22,8 +42,6 @@ import { ref } from 'vue';
 import { VueDraggable } from 'vue-draggable-plus';
 
 // https://vue-draggable-plus.pages.dev/
-
-const animation = ref<number>(150);
 
 const list1 = ref([
     {
@@ -57,5 +75,9 @@ function onAdd() {
 }
 function remove() {
     console.log('remove');
+}
+
+function onClicked() {
+    alert();
 }
 </script>
